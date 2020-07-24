cb1428a206b348f5e407faaa8edde0a76bfbc748
Rhuan Santos <50742592+rhuangabrielsantos@users.noreply.github.com>
2020-07-24 08:37:56 -0300

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Hello...</title>
+  <meta charset="utf-8"> 
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Hello World</h1>
+    </div>
+</body>
+</html>
